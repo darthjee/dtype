1368e884d775f5c48f6c5faccfcf122bed5b6b6b
darthjee <darthjee@gmail.com>
2016-08-04 15:25:50 -0300

    Source can write as well

--- ruby/lib/data/source.rb
+++ ruby/lib/data/source.rb
@@ -6,12 +6,44 @@ class Data::Source
   end
 
   def get
-    file.readline.to_f
+    case(mode)
+    when 'r'
+      try_read
+    when 'w'
+      generate_value
+    end
   end
 
   private
 
+  def mode
+    @mode ||= 'r'
+  end
+
+  def try_read
+    read_value
+  rescue EOFError
+    start_writing
+    generate_value
+  end
+
+  def start_writing
+    @mode = 'w'
+    file.close
+    @file = nil
+  end
+
+  def generate_value
+    Random.rand.tap do |value|
+      file.puts(value)
+    end
+  end
+
+  def read_value
+    file.readline.to_f
+  end
+
   def file
-    @file ||= File.open(file_path, 'r')
+    @file ||= File.open(file_path, mode)
   end
 end
