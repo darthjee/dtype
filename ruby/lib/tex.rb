bfe0254ad30c1bfac9dc93507901a8436495efe9
darthjee <darthjee@gmail.com>
2016-08-08 19:18:01 -0300

    Extract tex to template

--- ruby/lib/tex.rb
+++ ruby/lib/tex.rb
@@ -1,34 +1,4 @@
-Utils::Loader.require_cascade('utils/erb_builder')
+Utils::Loader.require_cascade('utils/template')
 
-class Tex
-  attr_reader :name, :input, :output, :variables
-
-  def initialize(name, input, output, variables)
-    @name = name
-    @input = input
-    @output = output
-    @variables = variables
-  end
-
-  def build
-    output_file.write erb_builder.result
-  end
-
-  private
-
-  def output_file
-    @output_file ||= File.open(output, 'w')
-  end
-
-  def erb_builder
-    @erb_builder ||= Utils::ErbBuilder.new(input_stream, variables)
-  end
-
-  def input_stream
-    @input_stream ||= input_file.read
-  end
-
-  def input_file
-    @input_file ||= File.open(input)
-  end
+class Tex < Utils::Template
 end
---
+++ ruby/lib/utils/template.rb
@@ -0,0 +1,34 @@
+Utils::Loader.require_cascade('utils/erb_builder')
+
+class Utils::Template
+  attr_reader :name, :input, :output, :variables
+
+  def initialize(name, input, output, variables)
+    @name = name
+    @input = input
+    @output = output
+    @variables = variables
+  end
+
+  def build
+    output_file.write erb_builder.result
+  end
+
+  private
+
+  def output_file
+    @output_file ||= File.open(output, 'w')
+  end
+
+  def erb_builder
+    @erb_builder ||= Utils::ErbBuilder.new(input_stream, variables)
+  end
+
+  def input_stream
+    @input_stream ||= input_file.read
+  end
+
+  def input_file
+    @input_file ||= File.open(input)
+  end
+end
