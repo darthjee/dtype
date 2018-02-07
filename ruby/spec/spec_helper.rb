a3e7169cf1b402b94f19f626e8bc4f9bd29ad432
darthjee <darthjee@gmail.com>
2018-02-07 09:36:20 +0100

    Add full coverage report capability

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,7 +1,11 @@
-require 'simplecov'
-SimpleCov.start
-require 'initializer'
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+  require 'initializer'
+  Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each { |f| require f }
+end
 
+require 'initializer'
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
