2c7c0f3c6a19b4fecf8770bde6c352af4654bd38
darthjee <darthjee@gmail.com>
2018-03-12 15:43:34 +0100

    Add simetrical variance calculation spec

--- ruby/spec/lib/data/transpose/binomial/result_spec.rb
+++ ruby/spec/lib/data/transpose/binomial/result_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Data::Transpose::Binomial::Result do
   let(:file) { File.open(file_path, 'r') }
+  let(:fixture_name) { 'binomial.dat' }
   let(:file_path) do
-    Utils::FilesLoader.file 'fixtures/data/binomial.dat'
+    Utils::FilesLoader.file "fixtures/data/#{fixture_name}"
   end
 
   subject do
@@ -14,11 +15,27 @@ describe Data::Transpose::Binomial::Result do
     it 'calculates the weighted average' do
       expect(subject.average).to eq(0.5102050000000001)
     end
+
+    context 'when we have a perfect gaussian' do
+      let(:fixture_name) { 'binomial_simple.dat' }
+
+      it 'calculates the weighted average' do
+        expect(subject.average).to eq(2)
+      end
+    end
   end
 
   describe '#variance' do
     it 'calculates the variance' do
       expect(subject.variance).to eq(0.010802497975000002)
+    end
+
+    context 'when we have a simetrical gaussian data' do
+      let(:fixture_name) { 'binomial_simple.dat' }
+
+      it 'calculates the variance' do
+        expect(subject.variance).to eq(0.25)
+      end
     end
   end
 end
