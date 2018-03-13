dd67daf453ef6c98baff0dd4c008b50f73063ca9
darthjee <darthjee@gmail.com>
2018-03-13 21:22:28 +0100

    Add squared experiments constants

--- ruby/lib/transpose.rb
+++ ruby/lib/transpose.rb
@@ -1,6 +1,9 @@
 class Transpose
-  BINOMIAL_SEGMENTS=Configuration.transpose.binomial.segments
-  BINOMIAL_TIMES=Configuration.transpose.binomial.times
-  BINOMIAL_THROWS=Configuration.transpose.binomial.throws
-  BINOMIAL_TOTAL = BINOMIAL_SEGMENTS * BINOMIAL_TIMES * BINOMIAL_THROWS
+  BINOMIAL_SEGMENTS = Configuration.transpose.binomial.segments
+  BINOMIAL_TIMES    = Configuration.transpose.binomial.times
+  BINOMIAL_THROWS   = Configuration.transpose.binomial.throws
+  BINOMIAL_TOTAL    = BINOMIAL_SEGMENTS * BINOMIAL_TIMES * BINOMIAL_THROWS
+
+  SQUARED_SEGMENTS  = Configuration.transpose.squared.segments
+  SQUARED_TIMES     = Configuration.transpose.squared.times
 end
