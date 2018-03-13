c75b247ee096813f147828f47774f6ebfbf8ad9e
darthjee <darthjee@gmail.com>
2018-03-13 23:30:03 +0100

    loader swallow error

--- ruby/spec/lib/utils/loader_spec.rb
+++ ruby/spec/lib/utils/loader_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
+RSpec.shared_examples 'a class loader' do |method|
+  context 'when creating from array' do
+    it 'returns the expected class' do
+      expect(subject.public_send(method)).to eq(Data::Transpose::Binomial::Experiment)
+    end
+  end
+
+  context 'when creating from string' do
+    let(:arguments) { 'data/transpose/binomial/experiment' }
+
+    it 'returns the expected class' do
+      expect(subject.public_send(method)).to eq(Data::Transpose::Binomial::Experiment)
+    end
+  end
+end
+
 RSpec.describe Utils::Loader do
   subject do
     described_class.new(arguments)
@@ -7,17 +23,27 @@ RSpec.describe Utils::Loader do
   let(:arguments) { %w(data transpose binomial experiment) }
 
   describe '#constantize' do
-    context 'when creating from array' do
-      it 'returns the expected class' do
-        expect(subject.constantize).to eq(Data::Transpose::Binomial::Experiment)
+    it_behaves_like 'a class loader', :constantize do
+      context 'when class do not exist' do
+        let(:arguments) { 'data/transposeee' }
+
+        it do
+          expect(subject.constantize).to be_nil
+        end
       end
     end
+  end
 
-    context 'when creating from string' do
-      let(:arguments) { 'data/transpose/binomial/experiment' }
+  describe '#constantize!' do
+    it_behaves_like 'a class loader', :constantize! do
+      context 'when class do not exist' do
+        let(:arguments) { 'data/transposeee' }
 
-      it 'returns the expected class' do
-        expect(subject.constantize).to eq(Data::Transpose::Binomial::Experiment)
+        it do
+          expect do
+            subject.constantize!
+          end.to raise_error(NameError)
+        end
       end
     end
   end
