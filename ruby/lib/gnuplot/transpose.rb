bcde67922414e01d6f6f8dc0681079f11f733813
darthjee <darthjee@gmail.com>
2018-03-25 19:19:22 +0200

    Add average graph

--- ruby/lib/gnuplot/transpose.rb
+++ ruby/lib/gnuplot/transpose.rb
@@ -1,8 +1,9 @@
 class Gnuplot::Transpose < Gnuplot::Base
-  TASKS=%w(squared binomial)
+  TASKS=%w(squared binomial average)
 
   autoload :Squared,  'gnuplot/transpose/squared'
   autoload :Binomial, 'gnuplot/transpose/binomial'
+  autoload :Average,  'gnuplot/transpose/average'
 
   def initialize(graph, input, output)
     super(:transpose, graph, input, output)
---
+++ ruby/lib/gnuplot/transpose/average.rb
@@ -0,0 +1,35 @@
+class Gnuplot::Transpose::Average < Gnuplot::Transpose
+  def initialize(input, output)
+    super(:average, input, output)
+  end
+
+  default_value :x_label, 'Random Number'
+  default_value :y_label, 'Frequency'
+
+  def plots
+    [
+      gauss_plot,
+      Gnuplot::Plot::Data.new({
+        input: file_name,
+        with: :boxes
+      })
+    ]
+  end
+
+  private
+
+  def gauss_plot
+    Gnuplot::Plot::Function.new(function: gauss)
+  end
+
+  def gauss
+    Data::Transpose::Binomial::Result.new(file).gaussian
+  end
+
+  def file
+    File.open(file_name, 'r')
+  end
+
+  default_value :file_name, Utils::FilesLoader.data.transpose.binomial.dat
+end
+
