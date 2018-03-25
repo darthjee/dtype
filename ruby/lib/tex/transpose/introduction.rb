af5fab0cfe267514af08bb8b88e2449753a459f6
darthjee <darthjee@gmail.com>
2018-03-25 21:45:32 +0200

    Make all the graphs objects

--- ruby/lib/tex/transpose/introduction.rb
+++ ruby/lib/tex/transpose/introduction.rb
@@ -1,8 +1,11 @@
 class Tex::Transpose::Introduction < Tex
+
   def variables
     {
       gaussian: Function::Gauss.new.tex,
-      squared_graph: squared_graph
+      squared_graph: squared_graph,
+      binomial_graph: binomial_graph,
+      average_graph: average_graph
     }
   end
 
@@ -13,6 +16,22 @@ class Tex::Transpose::Introduction < Tex
       file: Utils::FilesLoader.eps.transpose.squared,
       label: 'intro:squared',
       caption: 'Squared distribuition'
+    )
+  end
+
+  def binomial_graph
+    Tex::Graph.new(
+      file: Utils::FilesLoader.eps.transpose.binomial,
+      label: 'intro:binomial',
+      caption: 'Binomial experiment distribuition'
+    )
+  end
+
+  def average_graph
+    Tex::Graph.new(
+      file: Utils::FilesLoader.eps.transpose.average,
+      label: 'intro:average',
+      caption: 'Gaussian distribuition'
     )
   end
 end
